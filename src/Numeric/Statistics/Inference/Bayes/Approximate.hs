62905e8ca6fceefc17be4a44142765443cb45c0d
ocramz <zocca.marco@gmail.com>
2018-04-15 18:43:57 +0200

    add Approximate

---
+++ src/Numeric/Statistics/Inference/Bayes/Approximate.hs
@@ -0,0 +1,15 @@
+{-# language TypeFamilies #-}
+module Numeric.Statistics.Inference.Bayes.Approximate where
+
+import Control.Monad.Primitive (PrimMonad(..))
+
+import qualified System.Random.MWC.Probability as MWC
+
+
+
+
+
+
+
+
+  
