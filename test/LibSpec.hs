50348e8b26d0423a15f802f98cda50763a8beccb
ocramz <zocca.marco@gmail.com>
2019-07-23 23:15:41 +0200

    add alga

--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -27,8 +27,8 @@ spec =
 
 
 -- | Logistic map. Parameter bounds `0 < r < 4`, `0 < x0 < 1`, chaotic behaviour for r > 3.57 
--- logisticMap :: (Fractional a, Ord a, AdditiveGroup a, Multiplicative a) =>
---      Int -> a -> a -> Either String [a]
+logisticMap :: (Ord a, Num a) =>
+     Int -> a -> a -> Either String [a]
 logisticMap n r x0
   | r <= 0 || r >= 4 = Left "0 < r < 4"
   | x0 <= 0 || x0 >= 1 = Left "0 < x0 < 1"
