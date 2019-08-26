7741c9e889e8875d0a6d8bdeed41a54a87307d49
ocramz <ocramz@fripost.org>
2019-08-26 17:07:35 +0200

    keep track of factor size

--- src/Numeric/Statistics/Inference/Bayes/Exact/VariableElimination.hs
+++ src/Numeric/Statistics/Inference/Bayes/Exact/VariableElimination.hs
@@ -24,18 +24,20 @@ import Control.Monad.Catch (MonadThrow(..))
 -- import qualified Data.Massiv.Array as A (Array, all, Comp(..), makeArray, Construct(..), Sz(..))
 -- import Data.Massiv.Array (Index, Ix1(..), D, (..:), ifoldlWithin', foldlWithin', Lower, Dim(..), Source)
 -- mtl
-import Control.Monad.State (MonadState(..))
+import Control.Monad.State (MonadState(..), gets)
 -- permutation
 -- import qualified Data.Permute as P (permute, next, elems)
 -- transformers
-import Control.Monad.State (State(..), evalState)
--- import Control.Monad.Trans.State (StateT(..), runStateT, evalStateT)
+import Control.Monad.State (State(..), runState, evalState)
+import Control.Monad.Trans.State (StateT(..), runStateT, evalStateT)
 -- vector
 import qualified Data.Vector as V
 
+import Data.Permutation (Permutation, permutation, getPermutation, permutations)
+
 import Prelude hiding (lookup)
 
-import Data.Permutation (Permutation, permutation, getPermutation, permutations)
+
 
 
 student :: Graph Char
@@ -59,12 +61,11 @@ student =
     l = vertex 'l'
     s = vertex 's'
 
-{- |
 
-λ> permutations <$> TG.topSort student
 
-Just ["iscdgljh","sicdgljh","csidgljh","dscigljh","gscdiljh","lscdgijh","jscdglih","hscdglji","icsdgljh","idcsgljh","igcdsljh","ilcdgsjh","ijcdglsh","ihcdgljs","isdcgljh","isgdcljh","isldgcjh","isjdglch","ishdgljc","iscgdljh","isclgdjh","iscjgldh","ischgljd","iscdlgjh","iscdjlgh","iscdhljg","iscdgjlh","iscdghjl","iscdglhj"]
--}
+-- permutations <$> TG.topSort student
+
+-- Just ["iscdgljh","sicdgljh","csidgljh","dscigljh","gscdiljh","lscdgijh","jscdglih","hscdglji","icsdgljh","idcsgljh","igcdsljh","ilcdgsjh","ijcdglsh","ihcdgljs","isdcgljh","isgdcljh","isldgcjh","isjdglch","ishdgljc","iscgdljh","isclgdjh","iscjgldh","ischgljd","iscdlgjh","iscdjlgh","iscdhljg","iscdgjlh","iscdghjl","iscdglhj"]
 
 
 -- data SumProduct a =
@@ -72,22 +73,39 @@ Just ["iscdgljh","sicdgljh","csidgljh","dscigljh","gscdiljh","lscdgijh","jscdgli
 --   | Product (S.Set (Factor a))
 
 type VarId = Int
+data Temp = Temp { varId :: VarId, maxFactorSize :: Int } deriving (Eq, Show)
 -- newtype Lex m a = Lex { unLex :: StateT VarId m a } deriving (Functor, Applicative, Monad, MonadState VarId)
 -- runLex :: Monad m => Lex m a -> m a
 -- runLex lx = evalStateT (unLex lx) 0
-newtype Lex a = Lex { unLex :: State VarId a } deriving (Functor, Applicative, Monad, MonadState VarId)
-runLex :: Lex a -> a
-runLex lx = evalState (unLex lx) 0
+newtype Lex a = Lex { unLex :: State Temp a } deriving (Functor, Applicative, Monad, MonadState Temp)
 
-insert :: a -> IM.IntMap a -> Lex (IM.IntMap a)
-insert x mx = do
-  k <- get
+initTemp :: Temp
+initTemp = Temp 0 0
+
+runLex :: Lex a -> (a, Temp)
+runLex lx = runState (unLex lx) initTemp
+
+-- | Insert a new factor into scope and update the maximum size of scopes seen so far
+insertFactor :: Factor a -> IM.IntMap (Factor a) -> Lex (IM.IntMap (Factor a))
+insertFactor x mx = do
+  Temp k mfs <- get
   let mx' = IM.insert k x mx
-  put $ succ k
+      sz = factorSize x
+  put $ Temp (succ k) (max sz mfs)
   pure mx'
 
-fromList :: Foldable t => t a -> Lex (IM.IntMap a)
-fromList xs = foldlM (flip insert) IM.empty xs
+factorSize :: Factor a -> Int
+factorSize = length
+
+
+fromList :: Foldable t => t (Factor a) -> Lex (IM.IntMap (Factor a))
+fromList xs = foldlM (flip insertFactor) IM.empty xs
+
+
+-- -- mooo g = do
+-- --   ps <- permutations <$> TG.topSort g
+-- --   pure $ (elims g . getPermutation ) `map` ps
+
 
 -- | sequential sum-product elimination of graph factors
 -- 
@@ -96,24 +114,24 @@ fromList xs = foldlM (flip insert) IM.empty xs
 elims :: (TG.ToGraph g, Foldable t, Ord (TG.ToVertex g)) =>
          g
       -> t (TG.ToVertex g)
-      -> IM.IntMap (Factor (TG.ToVertex g))
+      -> (IM.IntMap (Factor (TG.ToVertex g)), Temp)
 elims g vs = runLex $ do
-  im0 <- factorIM g
+  let im0 = factorIM g
   foldlM (flip spe) im0 vs
 
 factorIM :: (TG.ToGraph g, Ord (TG.ToVertex g)) =>
             g
-         -> Lex (IM.IntMap (Factor (TG.ToVertex g)))
-factorIM g = do
-  im <- fromList $ TG.vertexList g
-  pure $ IM.map (`moralFactor` g) im
+         -> IM.IntMap (Factor (TG.ToVertex g))
+factorIM g = IM.map (`moralFactor` g) im where
+  im = IM.fromList $ zip [0..] (TG.vertexList g) 
 
 -- | Sum-product elimination
 spe :: (Ord a) => a -> IM.IntMap (Factor a) -> Lex (IM.IntMap (Factor a))
-spe z pphi = insert tau pphiC where
-  pphi' = factorsContaining z pphi
-  pphiC = pphi `IM.difference` pphi'
-  tau = eliminate z pphi'
+spe z pphi = insertFactor tau pphiC
+  where
+    pphi' = factorsContaining z pphi
+    pphiC = pphi `IM.difference` pphi'
+    tau = eliminate z pphi'
 
 forgetIndices :: Ord a => IM.IntMap a -> S.Set a
 forgetIndices = S.fromList . map snd . IM.toList 
@@ -139,6 +157,10 @@ sumOver v f = Factor $ S.filter (/= v) $ scope f
 -- | Does the factor have a given variable in scope?
 hasInScope :: Ord a => a -> Factor a -> Bool
 hasInScope v f = not $ null $ scope f `S.intersection` S.singleton v
+
+factors :: (TG.ToGraph t, Ord (TG.ToVertex t)) =>
+           t -> [Factor (TG.ToVertex t)]
+factors g = (`moralFactor` g) `map` TG.vertexList g
 
 moralFactor :: (TG.ToGraph g, Ord (TG.ToVertex g)) =>
                TG.ToVertex g -> g -> Factor (TG.ToVertex g)
