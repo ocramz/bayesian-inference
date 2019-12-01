54aaf1d4b9dc4cf101fdebe1da38c3173b48354a
ocramz <zocca.marco@gmail.com>
2019-12-01 23:04:52 +0100

    pin alga to 0.4

--- src/Numeric/Statistics/Nonparametric.hs
+++ src/Numeric/Statistics/Nonparametric.hs
@@ -9,42 +9,47 @@ import qualified Data.IntMap as IM
 -- mwc-probability
 import Control.Monad (replicateM)
 import System.Random.MWC.Probability (Prob, create, sample, samples, Gen, GenIO, GenST, categorical)
+import System.Random.MWC.Probability (gamma, normal)
 -- primitive
 import Control.Monad.Primitive (PrimState(..), PrimMonad(..))
 
 
 
-py :: (PrimMonad f) =>
-      Double
-   -> Double
-   -> Int
-   -> Gen (PrimState f)
-   -> f (Tables (Sum Integer))
-py a b n gen = go initial (n - 1) where
-  go acc 0 = pure acc
-  go acc i = do
-    acc' <- sample (pitmanYor a b acc) gen
-    go acc' (i - 1)
+pitmanYor :: (PrimMonad f) =>
+             Double -- ^ a \in [0, 1]
+          -> Double -- ^ b > 0
+          -> Int    -- ^ number of samples
+          -> Gen (PrimState f)
+          -> f [Integer]
+pitmanYor a b n gen = do
+  ts <- go initial (n - 1)
+  pure $ map getSum $ customers ts
+  where
+    go acc 0 = pure acc
+    go acc i = do
+      acc' <- sample (pitmanYorSingle a b acc) gen
+      go acc' (i - 1)
 
 
-pitmanYor :: (PrimMonad m, Integral a) =>
-             Double -- a \in [0, 1]
-          -> Double -- b > 0
-          -> Tables (Sum a)
-          -> Prob m (Tables (Sum a))
-pitmanYor a b zs = do
-  zn1 <- categorical (pms <> [pm1])
+pitmanYorSingle :: (PrimMonad m, Integral a) =>
+                   Double -- a \in [0, 1]
+                -> Double -- b > 0
+                -> Tables (Sum a)
+                -> Prob m (Tables (Sum a))
+pitmanYorSingle a b zs = do
+  zn1 <- categorical probs
   pure $ insert zn1 zs
   where
     m = fromIntegral $ uniques zs
     n = fromIntegral $ numCustomers zs
     d = n + b
+    probs = pms <> [pm1]
     pm1 = (m * a + b) / d
     pms = map (\x -> (fromIntegral (getSum x) - a) / d) $ customers zs
 
 
 
-
+-- | Tables at the Chinese Restaurant
 newtype Tables c = Tables {
   getTables :: IM.IntMap c } deriving (Eq, Show, Functor, Foldable)
 
@@ -77,3 +82,21 @@ kthTable :: Num a => Int -> Tables (Sum a) -> Maybe a
 kthTable k ts
   | k >= 0 = Just $ kthTableUnsafe k ts
   | otherwise = Nothing 
+
+
+--
+
+
+-- | Sampler for a Normal-Gamma random variate
+--
+-- https://en.wikipedia.org/wiki/Normal-gamma_distribution
+normalGamma :: PrimMonad m =>
+               Double -- ^ a > 0 
+            -> Double -- ^ b > 0
+            -> Double -- ^ mu
+            -> Double -- ^ lambda > 0 
+            -> Prob m Double
+normalGamma a b mu lambda = do
+  tau <- gamma a b
+  let stdd = sqrt $ 1 / (lambda * tau)
+  normal mu stdd
