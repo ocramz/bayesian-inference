79c2c646b6a8856fcc429bf4f9a924babf6530df
ocramz <zocca.marco@gmail.com>
2019-12-30 14:14:34 +0100

    add sampling dep

--- app/stat-rethinking/Main.hs
+++ app/stat-rethinking/Main.hs
@@ -10,26 +10,29 @@ main :: IO ()
 main = putStrLn "hello!"
 
 
-
-
-
--- | Ratio of observed outcomes to all possible outcomes for samples of length 3
+-- | Posterior probability of each hypothesis
 --
--- >>> pOutcomes (fromList [1,0,1]) (fromList [1,1,0,0])
--- 1 % 8
--- >>> pOutcomes (fromList [1,0,1]) (fromList [1,0,0,0])
--- 3 % 64
-pOutcomes :: Sample Int -- ^ observed sample
-          -> Sample Int -- ^ hypothesis
-          -> Ratio Int
-pOutcomes spl hypot = nsps % ntot
+-- >>> posterior (fromList [1,0,1]) [fromList [0,0,0,0], fromList [1,0,0,0], fromList [1,1,0,0], fromList [1,1,1,0], fromList [1,1,1,1]]
+-- [0 % 1,3 % 20,2 % 5,9 % 20,0 % 1]
+posterior :: Sample Integer -> [Sample Integer] -> [Ratio Integer]
+posterior spl hyps = map (% n) ws
   where
-    xs = samples3 hypot
-    nsps = length $ filter (== spl) xs
-    ntot = length xs
+    n = sum ws
+    ws = nWays spl hyps
+
+-- | Number of ways in which the observed sample could be produced by each of the "hypotheses"
+--
+-- >>> nWays (fromList [1,0,1]) [fromList [0,0,0,0], fromList [1,0,0,0], fromList [1,1,0,0], fromList [1,1,1,0], fromList [1,1,1,1]]
+-- [0,3,8,9,0]
+nWays :: Sample Integer -- ^ observed sample
+      -> [Sample Integer] -- ^ hypothesis space
+      -> [Integer]
+nWays spl hyps = map f hyps
+  where
+    f h = fromIntegral $ length $ filter (== spl) $ samples3 h
 
 -- | All possible samples of length 3
-samples3 :: Sample Int -> Sample (Sample Int)
+samples3 :: Sample a -> Sample (Sample a)
 samples3 xs = sample3 <$> xs <*> xs <*> xs
 
 
